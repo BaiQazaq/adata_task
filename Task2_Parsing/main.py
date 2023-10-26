25c22594d0e578aa80696de12680d5dbf6a01b3d
Isa Bahyt <assib79@gmail.com>
2023-10-26 22:53:00 +0600

    Updated

--- Task2_Parsing/main.py
+++ Task2_Parsing/main.py
@@ -6,36 +6,45 @@ from data_preparing import headers
 
 
 with open("units.json") as f:
-    units_50 = json.load(f)
+    units = json.load(f)
 
 cnt = 0
 all_units = []
+err = {}
 
 # перебираем все ссылки по компаниям и заходим на каждую из них и делаем выборку необходимых данных
-for name, href in units_50.items():
-    req = requests.get(url=href, headers=headers, verify=False)
-    src = req.text
-    soup = BeautifulSoup(src, "lxml")
-    try:
-        idbin = soup.find("th", text="БИН участника").find_next().text
-    except AttributeError:
-        print(AttributeError)
-        with open(f"data/{cnt}_{name}.html", "w") as f: # в случае срабатывания ошибки сохраняем код страницы, чтобы разобраться в характере ошибки
-            f.write(src)
-    boss = soup.find("th", text="ФИО").find_next().text
-    boss_inn = soup.find("th", text="ИИН").find_next().text
-    address = soup.find("th", text="Тип адреса").find_next("td").find_next().find_next().text.strip()
-    all_units.append({
-        "Наименование организации" : name,
-        "БИН организации": idbin,
-        "ФИО руководителя": boss,
-        "ИИН руководителя":boss_inn,
-        "Полный адресс": address
-    })
+def collect_info(dict):
+    for name, href in dict.items():
+        req = requests.get(url=href, headers=headers, verify=False)
+        src = req.text
+        soup = BeautifulSoup(src, "lxml")
+        try:
+            idbin = soup.find("th", text="БИН участника").find_next().text
+            boss = soup.find("th", text="ФИО").find_next().text
+            boss_inn = soup.find("th", text="ИИН").find_next().text
+            address = soup.find("th", text="Тип адреса").find_next("td").find_next().find_next().text.strip()
+            all_units.append({
+                "Наименование организации" : name,
+                "БИН организации": idbin,
+                "ФИО руководителя": boss,
+                "ИИН руководителя":boss_inn,
+                "Полный адресс": address
+            })
+        except AttributeError:
+            print(AttributeError)
+            err.update({name: href})
+            
+        cnt += 1
+        time.sleep(1)
+        if cnt % 10 == 0:
+            time.sleep(5)
+        if cnt % 50 == 0:
+            time.sleep(30)
 
-    cnt += 1
-    if cnt % 20 == 0:
-        time.sleep(20)
+collect_info(units)
+
+with open("data/err.json", "w") as f:
+    json.dump(err, f, indent=4, ensure_ascii=False)
 
 # создаем объект Pandas 
 df = pd.DataFrame(all_units)
